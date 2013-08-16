9b0ae6b98dd21c435c8ea3f95f628fddb439846d
Eduardo Zaghi <eduardozaghi@gmail.com>
2013-08-16 17:06:11 -0300

    helper to upload link

---
+++ spec/helpers/upload_helper_spec.rb
@@ -0,0 +1,36 @@
+#encoding: utf-8
+
+require 'spec_helper'
+
+describe UploadHelper do
+	context 'when object is persisted' do
+		let(:project){ Project.create}
+
+		subject{ helper.upload_link('Upload Imagem', project, '.images') }
+		
+		it{ should =~ />Upload Imagem</ }
+		it{ should =~ /data-image-container=".images"/ }
+		it{ should =~ /data-model="Project"/ }
+		it{ should =~ /data-model-id="#{project.id}"/ }
+		it{ should =~ /data-file-picker-key="#{PICKER_KEY}"/ }
+	end
+
+	context 'when project is new_record' do
+		let(:project){ Project.new}
+
+		subject{ helper.upload_link('Upload Imagem', project, '.images') }
+
+		it { should be_nil}
+	end
+
+	context 'when project is nil' do
+		subject{ helper.upload_link('Upload Imagem', nil, '.images') }
+
+		it { should be_nil}
+	end
+end
+
+class Project
+	include Mongoid::Document
+	include Mongoid::LolMultipleImages
+end
