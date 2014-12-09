7093fc2e077d3b016d9dd5d5dd8dcce4014d03e7
Eduardo Zaghi <eduardozaghi@gmail.com>
2014-12-09 10:54:04 -0200

    setting right configs for spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -34,19 +34,10 @@ RSpec.configure do |config|
   # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
 
+  config.filter_run_excluding :orm => 'active_record'
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
-
-  require 'database_cleaner'
-  config.before :suite do
-    DatabaseCleaner.strategy = :truncation
-    DatabaseCleaner.orm = "mongoid"
-  end
-
-  config.after do
-    DatabaseCleaner.clean
-  end
 end
