75514734b75449e2dc7e3b9311b340055616c245
Eduardo Zaghi <eduardozaghi@gmail.com>
2014-12-09 11:20:25 -0200

    removing InstanceMethods from lol_multiple_images

--- lib/active_record/lol_multiple_images.rb
+++ lib/active_record/lol_multiple_images.rb
@@ -8,19 +8,17 @@ module ActiveRecord
       has_many :photos, as: :photable
     end
 
+    def photo_cover(version=nil)
+      photo = self.photos.prioritized.first || photos.build
+
+      photo.image.url(version)
+    end
+
     module ClassMethods
       def multiple_uploads(option={})
         self.caption_status           = option[:caption] || false # config
         self.caption_localized_status = option[:caption_localized] || false
         self.available_locales        = option[:available_locales] || I18n.available_locales
-      end
-    end
-
-    module InstanceMethods
-      def photo_cover(version=nil)
-        photo = self.photos.prioritized.first || photos.build
-
-        photo.image.url(version)
       end
     end
   end
